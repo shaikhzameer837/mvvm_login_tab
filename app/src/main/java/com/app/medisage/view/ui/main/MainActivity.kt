99668b2c5ace313b2469bf7bb845639b98455b15
Zameer_shaikh <shaikhzameer837@gmail.com>
2022-05-09 13:22:58 +0530

    minor change

--- app/src/main/java/com/app/medisage/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/medisage/view/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.app.medisage.databinding.ActivityMainBinding
+import com.app.medisage.view.ui.adapter.SectionsPagerAdapter
 import com.google.android.material.tabs.TabLayout
 
 
